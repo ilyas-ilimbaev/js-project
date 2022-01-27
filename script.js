4bed45de378ad39835f46184492c54a6927fc7e8
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-01-28 02:10:40 +0500

    Modifided script.js file

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-alert("Hello world!");
-console.log("Hello world!");
+let title = "1 урок по js";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+console.log(title)
+alert("Первый урок");
