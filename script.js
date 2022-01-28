97020bfb9ee4ff8954abb4b2000b9738d28ea10c
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-01-28 21:33:24 +0500

    Lesson02 on js course

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let title = "1 урок по js";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "2 урок по js";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 2000;
+let rollback = 50;
+let fullPrice = 30000;
+let adaptive = true;
 
+//заголовок
 console.log(title);
-
-alert("Первый урок");
+//цена
+console.log(fullPrice);
+//наличие адаптивности
+console.log(adaptive);
+//вывод длины строки
+console.log("Длина строки" + ": " + screens.length);
+//вывод стоимости верстки экрана
+console.log("Cтоимость верстки экрана" + ": " + screenPrice + " рублей/ " + "384 долларов/ " + "11 094 гривен/ " + "244,329 юани");
+//вывод стоимости разработки сайта
+console.log("Cтоимость разработки сайта" + ": " + fullPrice + " рублей/ " + "384 долларов/ " + "11 094 гривен/ " + "244,329 юани");
+//работа со строкой screens
+console.log(screens.toLowerCase());
+console.log(screens.split("ы"));
+//вывод процент отката посреднику за работу
+console.log("Процент отката посреднику за работу" + ": " + fullPrice * (rollback/100));
