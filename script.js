1f8cc2d1a0378a3d5761d1990cdb89cd3188b109
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-02-04 19:46:54 +0500

    Second amendment in lesson branch number 3

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 'use strict';
 let title = prompt("Как называется ваш проект?");
-console.log(title);
 let screens = prompt("Какие типы экранов нужно разработать? Простые, Сложные или Интерактивные?");
-console.log(screens);
 let screensPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screensPrice);
-
-let adaptive = prompt("Нужен ли адаптив на сайте?");
-console.log(adaptive == "Да");
-
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service1);
 let servicePrice1 = +prompt("Сколько это будет стоить?");
-console.log(servicePrice1);
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service2);
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-console.log(servicePrice2);
-
 let fullPrice = screensPrice + servicePrice1 + servicePrice2;
-console.log("Стоимость " + fullPrice);
-
 let servicePercentPrice = fullPrice - (fullPrice / 100 * 30);
-console.log("Итоговая стоимость " + Math.ceil(servicePercentPrice));
 
-let terms;
 
-if (fullPrice > 30000) {
-    console.log(terms = "Даем скидку в 10%");
+if (fullPrice >= 30000) {
+    console.log("Даем скидку в 10%");
 
-} else if (fullPrice > 15000 && fullPrice < 30000){
-    console.log(terms = "Даем скидку в 5%");
+} else if (fullPrice >= 15000 && fullPrice < 30000){
+    console.log("Даем скидку в 5%");
 
-} else if (fullPrice < 15000 && fullPrice > 0){
-    console.log(terms = "Скидка не предусмотрена");
+} else if (fullPrice < 15000 && fullPrice >= 0){
+    console.log("Скидка не предусмотрена");
 
 } else if (fullPrice < 0) {
-    console.log(terms = "Что то пошло не так");
+    console.log("Что то пошло не так");
 }
+
+console.log(title);
+console.log(adaptive);
+console.log(screens);
+console.log(screensPrice);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log("Стоимость " + fullPrice);
+console.log("Итоговая стоимость " + Math.ceil(servicePercentPrice));
