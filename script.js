17c19ace7da768520386c401142ecfeff1e843be
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-02-05 00:50:36 +0500

    Edit lesson-03

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-let rollback = 30;
 let fullPrice = screensPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice / 100 * rollback);
+let rollback = fullPrice * (30 / 100);
+let servicePercentPrice = fullPrice - rollback;
 
 
 if (fullPrice >= 30000) {
