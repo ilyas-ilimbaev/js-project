63f4b756cbe0b1287954e0a40257a033e08cfd7d
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-02-06 18:56:00 +0500

    Ready task number 4

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 'use strict';
 
 let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать? (пример: 'Простые, Сложные, Интерактивные')");
+let screens = prompt("Какие типы экранов нужно разработать? Простые, Сложные или Интерактивные?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let allServicePrices;
-let fullPrice;
 let servicePercentPrice;
 let rollback = 30;
-
-
 
 const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
 };
+
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+};
+
+const getTitle = function() {
+    title = title.trim().toLowerCase();
+    return title = title[0].toUpperCase() + title.slice(1);
+};
+
+function getFullPrice() {
+    return screenPrice + allServicePrices;
+}
+
+function getServicePercentPrices(rollback) {
+    return fullPrice - (fullPrice * (rollback / 100));
+}
 
 const getRollbackMessage = function(price) {
     if(price >= 30000) {
@@ -31,23 +46,6 @@ const getRollbackMessage = function(price) {
     }
 };
 
-const getAllServicePrices = function() {
-    return servicePrice1 + servicePrice2;
-};
-
-function getFullPrice() {
-    return screenPrice + allServicePrices;
-}
-
-const getTitle = function() {
-    title = title.trim().toLowerCase();
-    return title = title[0].toUpperCase() + title.slice(1);
-};
-
-function getServicePercentPrices(rollback) {
-    return fullPrice - (fullPrice * (rollback / 100));
-}
-
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices(rollback);
@@ -56,8 +54,6 @@ showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
-
-
 console.log(getRollbackMessage(fullPrice));
 console.log(screens.toLowerCase().split(", "));
-console.log("Стоимость разработки сайта " + servicePercentPrice + " рублей");
+console.log("Итоговая стоимость " + servicePercentPrice + " рублей");
